bebd32ede93f1bf20291587507b666209b3c48f2
LEARN Academy <sdunlap102326@gmail.com>
2023-05-09 14:15:56 -0700

    Added create and read functionality

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,22 @@
 class ApartmentsController < ApplicationController
+    def index
+        apartments = Apartment.all
+        render json: apartments
 end
+
+def create
+    apartment = Apartment.create(apartment_params)
+    if apartment.valid?
+    render json: apartment
+    else
+    render json: apartment.errors, status: 422
+    end
+end
+
+private 
+def apartment_params 
+    params.require(:apartment).permit(:user_id, :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pet, :image, :ac_unit, :major_appliances, :parking, :hoa)
+end
+end
+
+
