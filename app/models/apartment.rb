0a9da9145104a54351f5c4374d80c10a026306e3
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-05-09 11:33:50 -0700

    rspec and validations for apartment

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,4 @@
 class Apartment < ApplicationRecord
   belongs_to :user
+  validates :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pet, :image, :ac_unit, :major_appliances, :parking, :hoa, presence: true
 end
